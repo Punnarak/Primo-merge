3584c342a8156eab8148eab1fd5de07debe69795
Punnarak <50368218+Punnarak@users.noreply.github.com>
2023-02-03 15:56:58 +0000

    ได้ 3

--- .build/index.js
+++ .build/index.js
@@ -3,9 +3,9 @@ const readline = require("readline-sync");
 var c1 = readline.question("Enter Collection_1 : ");
 var c2 = readline.question("Enter Collection_2 : ");
 let myFunc = (num) => Number(num);
-var collection_1 = Array.from(String(c1).split(" "), myFunc);
-var collection_2 = Array.from(String(c2).split(" "), myFunc);
-let merge = function(collection_12, collection_22) {
+var collection_1 = String(c1).split(" ").map(myFunc);
+var collection_2 = String(c2).split(" ").map(myFunc);
+function merge(collection_12, collection_22) {
   let result = collection_12.concat(collection_22);
   for (let i = 1; i < result.length; i++) {
     for (let j = 0; j < i; j++) {
@@ -17,6 +17,6 @@ let merge = function(collection_12, collection_22) {
     }
   }
   return result;
-};
+}
 console.log(merge(collection_1, collection_2));
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,13 +1,14 @@
+
 const readline = require("readline-sync");
 var c1 = readline.question("Enter Collection_1 : ");
 var c2  = readline.question("Enter Collection_2 : ");
 let myFunc = num => Number(num);
-var collection_1 = Array.from(String(c1).split(' '), myFunc);
-var collection_2 = Array.from(String(c2).split(' '), myFunc);
+
+var collection_1 = String(c1).split(' ').map(myFunc)
+var collection_2 = String(c2).split(' ').map(myFunc)
 
 
-
-let merge: ( collection_1: number[],  collection_2: number[]) => number[] = function ( collection_1: number[],  collection_2: number[]): number[]{
+function merge( collection_1: number[],  collection_2: number[]): number[]{
   
    let result = collection_1.concat(collection_2)
   for(let i = 1 ; i < result.length ; i++){
