c13f054d2baef6e75182f58f23c94b66e5f4d056
Punnarak <50368218+Punnarak@users.noreply.github.com>
2023-02-03 14:27:16 +0000

    ได้แล้วว

--- .build/index.js
+++ .build/index.js
@@ -1,2 +1,23 @@
 "use strict";
+const readline = require("readline-sync");
+var c1 = readline.question("Enter Collection_1 : ");
+var c2 = readline.question("Enter Collection_2 : ");
+let myFunc = (num) => Number(num);
+var collection_1 = Array.from(String(c1).split(" "), myFunc);
+var collection_2 = Array.from(String(c2).split(" "), myFunc);
+let merge = function(collection_12, collection_22) {
+  let result = collection_12.concat(collection_22);
+  console.log(result.length);
+  for (let i = 1; i < result.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (result[i] < result[j]) {
+        let temp = result[i];
+        result[i] = result[j];
+        result[j] = temp;
+      }
+    }
+  }
+  return result;
+};
+console.log(merge(collection_1, collection_2));
 //# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,28 @@
+const readline = require("readline-sync");
+var c1 = readline.question("Enter Collection_1 : ");
+var c2  = readline.question("Enter Collection_2 : ");
+let myFunc = num => Number(num);
+var collection_1 = Array.from(String(c1).split(' '), myFunc);
+var collection_2 = Array.from(String(c2).split(' '), myFunc);
+
+// console.log( collection_2);
+
+let merge: ( collection_1: number[],  collection_2: number[]) => number[] = function ( collection_1: number[],  collection_2: number[]): number[]{
+  
+   let result = collection_1.concat(collection_2)
+    console.log(result.length)
+  for(let i = 1 ; i < result.length ; i++){
+    for(let j = 0 ; j < i ; j++){
+      if(result[i] < result[j]){
+        let temp = result[i]
+        result[i] = result[j]
+        result[j] = temp
+        
+      }
+    }
+  }
+  
+  return result;
+}
+console.log(merge(collection_1,collection_2));
+
